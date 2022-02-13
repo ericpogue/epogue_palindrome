a1941b583a23d079595b37cfa9100bed6347f798
Eric Pogue <eric.pogue@ej4.com>
2022-02-13 11:00:57 -0600

    Factor into module, add integer palindrome detector

--- lib/epogue_palindrome.rb
+++ lib/epogue_palindrome.rb
@@ -1,17 +1,25 @@
 require "epogue_palindrome/version"
 
-class String
+module EpoguePalindrome
 
-  # Returns true for a palindrome, false otherwise.
-  def palindrome?
-    processed_content == processed_content.reverse
-  end
-
-
-  private
-
-    # Returns content for palindrome testing.
-    def processed_content
-      self.scan(/[a-z]/i).join.downcase
+    # Returns true for a palindrome, false otherwise.
+    def palindrome?
+      processed_content == processed_content.reverse
     end
+  
+  
+    private
+  
+      # Returns content for palindrome testing.
+      def processed_content
+        self.to_s.scan(/[a-z\d]/i).join.downcase
+      end
+end
+
+class String
+  include EpoguePalindrome
+end
+
+class Integer
+include EpoguePalindrome
 end
